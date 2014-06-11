1b1a253d4aafd70e612e3328d62b98c5a85f0e89
rrgayhart <rrgayhart@gmail.com>
2014-06-11 13:42:45 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlagDemoApp::Application.config.secret_key_base = '802c7d03c1ca9f2a1776e6cdd34be670ff13208353e17b2c0dbaad521a64dfe0b39858955429cd54e24fe779d4f783edcd8d6ee9eff1f828439d7ab2b9069606'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlagDemoApp::Application.config.session_store :cookie_store, key: '_flag_demo_app_session'
